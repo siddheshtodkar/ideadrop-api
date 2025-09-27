f9956ca13f69e83c089cd1600fb850b0601924eb
siddheshtodkar <stodkar53@gmail.com>
2025-09-27 11:57:23 +0530

    user schema

---
+++ models/user.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  }
+}, {
+  timestamps: true
+})
+
+// Hash passwords before saving
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password'))
+    return true
+
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+  next()
+})
+
+const User = mongoose.model('User', userSchema)
+
+export default User
